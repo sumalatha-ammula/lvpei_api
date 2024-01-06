082ae055be1a746f3ec311c496f7af7e301b9e1c
Tulasiram <ram20076@gmail.com>
2024-01-06 10:20:17 +0530

    Initial Commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,215 @@
+<?php
+declare(strict_types = 1)
+	;
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc.
+ * (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link https://cakephp.org CakePHP(tm) Project
+ * @since 0.2.9
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\Http\Response;
+use Cake\Utility\Text;
+use Cake\View\Exception\MissingTemplateException;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Static content controller
+ *
+ * This controller will render views from templates/Pages/
+ *
+ * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
+ */
+class AdminController extends AppController {
+	public function initialize(): void {
+		parent::initialize ();
+		$this->loadModel ( 'Admin' );
+		
+		$this->loadComponent ( 'Auth', array (
+				'loginAction' => array (
+						'controller' => 'Admin',
+						'action' => 'index' 
+				),
+				'unauthorizedRedirect' => array (
+						'controller' => 'Admin',
+						'action' => 'index' 
+				),
+				'logoutRedirect' => array (
+						'controller' => 'Admin',
+						'action' => 'index' 
+				),
+				'loginRedirect' => array (
+						'controller' => 'Admin',
+						'action' => 'dashboard' 
+				),
+				'authError' => 'Please log in to view your account.',
+				'authenticate' => array (
+						'Form' => array (
+								'userModel' => 'Admin',
+								'fields' => [ 
+										'username' => 'username',
+										'password' => 'password' 
+								] 
+						) 
+				),
+				'unauthorizedRedirect' => true 
+		) );
+		$this->admindata = $this->Auth->user ();
+		$actionname = $this->request->getParam ( 'action' );
+		$this->set ( "action", $actionname );
+	}
+	public function logout() {
+		return $this->redirect ( $this->Auth->logout () );
+	}
+	public function index() {
+		$this->viewBuilder ()->disableAutoLayout ();
+		/*
+		 * $pwd = "PoliticalLab2022";
+		 * $hasher = new DefaultPasswordHasher ();
+		 * $hashedpwd = $hasher->hash ( $pwd );
+		 * echo $hashedpwd;
+		 *
+		 * die;
+		 */
+		if ($this->request->is ( 'post' )) {
+			$user = $this->Auth->identify ();
+			if ($user) {
+				$this->Auth->setUser ( $user );
+				$redirecturl = [ 
+						"controller" => "Admin",
+						"action" => "dashboard" 
+				];
+				return $this->redirect ( $redirecturl );
+			} else {
+				$this->Flash->error ( __ ( 'Invalid username or password, try again' ) );
+				return $this->redirect ( array (
+						"controller" => "Admin",
+						"action" => "index" 
+				) );
+			}
+		}
+	}
+	public function dashboard() {
+	}
+	public function agents() {
+		$agentsdata = $this->Agent->find ( 'all' );
+		$agents = $this->paginate ( $agentsdata );
+		$this->set ( "agents", $agents );
+	}
+	public function createagent() {
+		
+		$agent = $this->Agent->newEmptyEntity ();
+		if ($this->request->is ( 'post' )) {
+			$agent = $this->Agent->patchEntity ( $agent, $this->request->getData () );
+			$hasher = new DefaultPasswordHasher ();
+			$agent->created_by = $this->admindata['id'];
+			//$agent->password = $hasher->hash ( $agent->password );
+			if ($this->Agent->save ( $agent )) {
+				$this->Flash->success ( __ ( 'The Agent has been saved.' ) );
+				return $this->redirect ( [
+						'action' => 'agents'
+				]);
+			}
+			$this->Flash->error(__('The agent could not be saved. Please, try again.'));
+		}
+		$this->set(compact ( 'agent' ) );
+	}
+	public function surveys() {
+		$surveydata = $this->Survey->find ( 'all' );
+		$surveys = $this->paginate ( $surveydata);
+		$this->set ( "surveys", $surveys);
+	}
+	
+	public function savesurveyquestionoptions(){
+		$this->response->withHeader ( 'Access-Control-Allow-Origin', '*' );
+		$this->response->withType ( 'json' );
+		$this->request->allowMethod ( [
+				'post',
+				'put',
+				'get'
+		] );
+		$result = [];
+		$this->viewBuilder ()->setLayout ( "ajax" );
+		if ($this->request->is ( 'post' )) {
+			$data=$this->request->getData();
+			
+			$sq = $this->SurveyQuestions->newEmptyEntity ();
+			$sq = $this->SurveyQuestions->patchEntity ( $sq, $data);
+			$sq->created_by = $this->admindata['id'];
+			if ($sqinsert = $this->SurveyQuestions->save ( $sq)) {
+				foreach($data['answers'] as $ans){
+					$so = $this->SurveyOptions->newEmptyEntity ();
+					$sodata = [];
+					$sodata['survey_questions_id'] = $sqinsert->id;
+					$sodata['answer'] = $ans;
+					$sop = $this->SurveyOptions->patchEntity ( $so, $sodata);
+					if(!$this->SurveyOptions->save ( $sop)){
+						debug($so->getErrors()); 
+					}
+				}
+			}
+			$result = $data;
+		}
+	
+		$this->set("result", $result);
+	}
+	
+	public function viewsurveyquestionoptions(){
+		$this->response->withHeader ( 'Access-Control-Allow-Origin', '*' );
+		$this->response->withType ( 'json' );
+		$this->request->allowMethod ( [
+				'post',
+				'put',
+				'get'
+		] );
+		$result = [];
+		$this->viewBuilder ()->setLayout ( "ajax" );
+		if ($this->request->is ( 'post' )) {
+			$data=$this->request->getData();
+			$surveyquestions = $this->SurveyQuestions->find('all')
+			->where(['SurveyQuestions.id' => $data['survey_id']])
+			->contain(['SurveyOptions'])
+			->toArray();
+			$result['surveyquestions'] = $surveyquestions;
+		}
+		$this->set("result", $result);
+	}
+	
+	public function createsurvey() {
+		$survey = $this->Survey->newEmptyEntity ();
+		if ($this->request->is ( 'post' )) {
+			$survey= $this->Survey->patchEntity ( $survey, $this->request->getData () );
+			$survey->created_by = $this->admindata['id'];
+			$survey->category_id = 1;
+			if ($this->Survey->save ( $survey)) {
+				$this->Flash->success ( __ ( 'The Survey has been saved.' ) );
+				return $this->redirect ( [
+						'action' => 'surveys'
+				]);
+			}
+			$this->Flash->error(__('The Survey could not be saved. Please, try again.'));
+		}
+		$this->set(compact ( 'survey' ) );
+	}
+	public function createsurveyquestion() {
+	}
+	public function surveydata() {
+	    $surveydata = $this->SurveyReport->find ( 'all' )
+		->contain(['SurveyQuestions', 'SurveyReportData']);
+		debug($surveydata->toArray());die;
+	}
+}
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,55 +1,166 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
-    <?= $this->Html->meta('icon') ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>LV Prasad Eye Hospital - Ravi</title>
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <?= $this->Html->css(['summernote-lite.min', 'adminlte.min','umespace']); ?>
+  <?php echo $this->Html->script(['jquery.min', 'bootstrap.bundle.min','jquery.validate.min', 'additional-methods.min','summernote-lite.min', 'adminlte']); ?>
+  <?= $this->fetch('meta') ?>
+  <?= $this->fetch('css') ?>
+  <?= $this->fetch('script') ?>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link href='//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/css/dataTables.bootstrap4.min.css" />
+<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.1/summernote.css" rel="stylesheet">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/jquery.dataTables.min.js"></script>
+<script>
+  var baseURL = "<?php echo $this->Url->build('/', array('fullBase'=>true)); ?>";
+  </script>
+<style>
+.card-header .card-tools {
+	float: none;
+}
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
-
-    <?= $this->fetch('meta') ?>
-    <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
+.card-header .card-tools a.btn-block {
+	float: right;
+	width: auto;
+	display: inline-block;
+}
+</style>
 </head>
-<body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+<div class="wrapper">
+	<!-- Navbar -->
+	<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+		<!-- Left navbar links -->
+		<ul class="navbar-nav">
+			<li class="nav-item"><a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a></li>
+		</ul>
+		<!-- Right navbar links -->
+		<ul class="navbar-nav ml-auto">
+			<!-- Navbar Search -->
+			</a>
+			<div class="navbar-search-block">
+				<form class="form-inline">
+					<div class="input-group input-group-sm">
+						<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+						<div class="input-group-append">
+							<button class="btn btn-navbar" type="submit">
+								<i class="fas fa-search"></i>
+							</button>
+							<button class="btn btn-navbar" type="button" data-widget="navbar-search">
+								<i class="fas fa-times"></i>
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			</li>
+			<!-- Messages Dropdown Menu -->
+			<!-- Notifications Dropdown Menu -->
+			<li class="nav-item dropdown"><a class="nav-link" data-toggle="dropdown" href="#"> <i class="far fa-bell"></i> <span class="badge badge-warning navbar-badge">15</span>
+			</a>
+				<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+					<span class="dropdown-item dropdown-header">15 Notifications</span>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item"> <i class="fas fa-envelope mr-2"></i> 4 new messages <span class="float-right text-muted text-sm">3 mins</span>
+					</a>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item"> <i class="fas fa-users mr-2"></i> 8 friend requests <span class="float-right text-muted text-sm">12 hours</span>
+					</a>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item"> <i class="fas fa-file mr-2"></i> 3 new reports <span class="float-right text-muted text-sm">2 days</span>
+					</a>
+					<div class="dropdown-divider"></div>
+					<a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+				</div></li>
+			<li class="nav-item"><a class="nav-link" data-widget="fullscreen" href="#" role="button"> <i class="fas fa-expand-arrows-alt"></i>
+			</a></li>
+			<li class="nav-item"><a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button"> <i class="fas fa-th-large"></i>
+			</a></li>
+		</ul>
+	</nav>
+	<!-- /.navbar -->
+	<!-- Main Sidebar Container -->
+	<aside class="main-sidebar sidebar-dark-primary elevation-4">
+		<!-- Brand Logo -->
+		<a href="#" class="brand-link"> <span class="brand-text font-weight-light">Political Laboratory</span>
+		</a>
+		<div class="sidebar">
+			<nav class="mt-2">
+				<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+					
+					<li class="nav-item "><a href="dashboard" class="nav-link <?php echo ($action == 'dashboard')?'active':''; ?>"> <i class="nav-icon fas fa-tachometer-alt"></i>
+							<p>Dashboard</p>
+					</a></li>
+					<li class="nav-item">
+					<?php
+					echo $this->Html->link ( '<i class="nav-icon fas fa-user"></i><p>Agents</p>', [ 
+							'controller' => "admin",
+							"action" => "agents" 
+					], [ 
+							'escape' => false,
+							'class' => 'nav-link ' . (($action == 'agents') ? 'active' : '') 
+					] );
+					?>
+		            </li>
+		            <li class="nav-item">
+					<?php
+					echo $this->Html->link ( '<i class="nav-icon fas fa-user"></i><p>Surveys</p>', [ 
+							'controller' => "admin",
+							"action" => "surveys" 
+					], [ 
+							'escape' => false,
+							'class' => 'nav-link ' . (($action == 'surveys') ? 'active' : '') 
+					] );
+					?>
+		            </li>
+		            <li class="nav-item">
+					<?php
+					echo $this->Html->link ( '<i class="nav-icon fas fa-user"></i><p>Survey Data</p>', [ 
+							'controller' => "admin",
+							"action" => "surveydata" 
+					], [ 
+							'escape' => false,
+							'class' => 'nav-link ' . (($action == 'surveydata') ? 'active' : '') 
+					] );
+					?>
+		            </li>
+		           
+					<li class="nav-item">
+            <?php
+												echo $this->Html->link ( '<i class="nav-icon fas fa-sign-in-alt"></i><p>Logout</p>', [ 
+														'controller' => "admin",
+														"action" => "logout" 
+												], [ 
+														'escape' => false,
+														'class' => 'nav-link' 
+												] );
+												?>
+    		</li>
+				</ul>
+			</nav>
+		</div>
+	</aside>
+	<!-- Content Wrapper. Contains page content -->
+	<div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header"></section>
+		<div class="content">
+			<div class="container-fluid">
+    <?= $this->Flash->render() ?>
+     <?= $this->fetch('content') ?>
+   </div>
+		</div>
+	</div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 </body>
 </html>
