26aea6229fd9e0abcdb420750f2a390de6d350d8
Tulasiram <ram20076@gmail.com>
2024-01-16 13:19:09 +0530

    DB Script for Survey data

---
+++ DB/survey_data.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jan 16, 2024 at 07:47 AM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lvpeiravi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey_data`
+--
+
+CREATE TABLE `survey_data` (
+  `id` bigint(255) NOT NULL,
+  `survey_id` bigint(33) NOT NULL,
+  `survey_questions_id` bigint(33) NOT NULL,
+  `field_executive_id` bigint(33) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `geo_location` varchar(255) NOT NULL,
+  `survey_question` text,
+  `option_data` varchar(255) NOT NULL,
+  `sync_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `partcipants_id` bigint(33) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `survey_data`
+--
+ALTER TABLE `survey_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `survey_data`
+--
+ALTER TABLE `survey_data`
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
